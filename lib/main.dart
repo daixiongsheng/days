bc49d8d59c4c0cce424896fb0e3bae3913802b2d
daixiongsheng <daixiongsheng@bytedance.com>
2023-12-15 00:59:40 +0800

    feat: init drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<String> loadAsset() async {
+  return await rootBundle.loadString('assets/config.json');
+}
 
 void main() {
   runApp(const MyApp());
+}
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New route"),
+      ),
+      body: const Center(
+        child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+class CupertinoTestRoute extends StatelessWidget {
+  const CupertinoTestRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.black12,
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text("Cupertino Demo"),
+      ),
+      child: Center(
+        child: CupertinoButton(
+            color: CupertinoColors.activeBlue,
+            child: const Text("Press"),
+            onPressed: () {}),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,101 +52,255 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/',
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (context) {
+          String routeName = settings.name!;
+          // 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
+          // 引导用户登录；其他情况则正常打开路由。
+          print('routeName$routeName');
+          return NewRoute();
+        });
+      },
+      routes: {
+        "home": (context) => const DefaultTabController(
+              length: 3,
+              child: Scaffold(),
+            ),
+        "new_page": (context) => NewRoute(),
+        "tip": (context) => TipRoute(
+              text: ModalRoute.of(context)!.settings.arguments as String,
+            ),
+        "/": (context) =>
+            const MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+      },
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class TipRoute extends StatelessWidget {
+  const TipRoute({
+    super.key,
+    required this.text, // 接收一个text参数
+  });
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("提示"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: const Text("返回"),
+              ),
+              Image.asset('assets/images/logo.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDrawerHeader extends DrawerHeader {
+  const CustomDrawerHeader({
+    super.key,
+    super.decoration,
+    required super.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterial(context));
+    assert(debugCheckHasMediaQuery(context));
+    final ThemeData theme = Theme.of(context);
+    final double statusBarHeight = MediaQuery.paddingOf(context).top;
+    return SizedBox(
+      height: statusBarHeight + 160.0 + 1.0,
+      child: AnimatedContainer(
+        padding: padding.add(EdgeInsets.only(top: statusBarHeight)),
+        decoration: decoration,
+        duration: duration,
+        curve: curve,
+        child: child == null
+            ? null
+            : DefaultTextStyle(
+                style: theme.textTheme.bodyLarge!,
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: child!,
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  static const Color hoverColor = Colors.grey;
+  static const Color drawerBackgroundColor = Color(0xFF222222);
+  static const TextStyle textStyle = TextStyle(color: Colors.white);
+
+  List<Widget> titles = [
+    ListTile(
+      textColor: textStyle.color,
+      title: const Text(
+        "Home22",
+      ),
+      hoverColor: hoverColor,
+      onTap: () {
+        print("Home tap");
+      },
+      selectedTileColor: Colors.black,
+    ),
+    ListTile(
+      textColor: textStyle.color,
+      title: const Text("Home"),
+      hoverColor: hoverColor,
+      onTap: () {
+        print("Home tap");
+      },
+      selected: false,
+    ),
+    ListTile(
+      textColor: textStyle.color,
+      title: const Text("Home"),
+      hoverColor: hoverColor,
+      onTap: () {
+        print("Home tap");
+      },
+      selected: false,
+    ),
+    ListTile(
+      textColor: textStyle.color,
+      hoverColor: hoverColor,
+      title: const Text("Home"),
+      onTap: () {
+        print("Home tap");
+      },
+      selected: false,
+    ),
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    loadAsset().then((value) {
+      print('loadAsset$value');
+    });
+    //导航到新路由
+    // Navigator.of(context)
+    //     .push(
+    //   MaterialPageRoute(
+    //       builder: (context) {
+    //         // return NewRoute();
+    //         print('object');
+    //         return const TipRoute(
+    //           // 路由参数
+    //           text: "我是提示xxxx",
+    //         );
+    //       },
+    //       // fullscreenDialog: true,
+    //       maintainState: false),
+    // )
+    //     .then((value) {
+    //   print("我是返回: $value");
+    // });
+
+    Navigator.of(context).pushNamed("tip", arguments: "hello");
+  }
+
+  late AppBar appBar = AppBar(
+    backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    title: Text(
+      widget.title,
+      style: textStyle,
+      textAlign: TextAlign.end,
+    ),
+  );
+
+  late ListView listView = ListView(
+    padding: EdgeInsets.zero,
+    physics: const NeverScrollableScrollPhysics(),
+    children: [
+      const CustomDrawerHeader(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/a.png'), fit: BoxFit.fill),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "账号信息",
+                style: textStyle,
+              )
+            ],
+          )),
+      ...titles,
+    ],
+  );
+
+  late Drawer drawer = Drawer(
+    backgroundColor: drawerBackgroundColor,
+    width: MediaQuery.of(context).size.width * 0.8,
+    // clipBehavior: Clip.none,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.zero),
+    ),
+    child: listView,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      drawer: drawer,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
+            ),
+            const DefaultTextStyle(
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.left,
+              child: Text.rich(TextSpan(
+                  children: [
+                    TextSpan(
+                        text: "span1", style: TextStyle(color: Colors.black))
+                  ],
+                  text: AutofillHints.addressCity,
+                  style: TextStyle(color: Colors.red))),
             ),
             Text(
               '$_counter',
@@ -119,7 +313,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
